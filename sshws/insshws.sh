1de8e0a0e09dc3a43ee6e759dbc878da2c013e30
aerosmith29 <82898813+aerosmith29@users.noreply.github.com>
2024-03-15 22:25:13 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -17,7 +17,7 @@ chmod +x /usr/local/bin/ws-ovpn
 
 
 #System OpenSSH Websocket-SSH Python
-#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
+#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/aerosmith29/b/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/aerosmith29/b/main/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
